9cad298dd3957526564d3be1f9184187fbcbdac2
Ahmad Bawaneh <akabme@gmail.com>
2018-06-23 03:47:30 +0300

    add AUTHERS, use mycila license plugin, remove checkstyle, fix module.gwt.xml, remove ie related code

--- src/test/java/org/gwtproject/timer/client/TimerTest.java
+++ src/test/java/org/gwtproject/timer/client/TimerTest.java
@@ -1,57 +1,73 @@
+/*
+ * Copyright © 2018 The GWT Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gwtproject.timer.client;
 
 import com.google.gwt.junit.client.GWTTestCase;
 
 public class TimerTest extends GWTTestCase {
-    @Override
-    public String getModuleName() {
-        return "org.gwtproject.timer.client.TimerTest";
-    }
+  @Override
+  public String getModuleName() {
+    return "org.gwtproject.timer.Timer";
+  }
 
-    public void testTimerSchedule(){
-        delayTestFinish(500);
-        Timer timer=new Timer() {
-            @Override
-            public void run() {
-                finishTest();
-            }
+  public void testTimerSchedule() {
+    delayTestFinish(500);
+    Timer timer =
+        new Timer() {
+          @Override
+          public void run() {
+            finishTest();
+          }
         };
-        timer.schedule(100);
-    }
+    timer.schedule(100);
+  }
 
-    public void testTimerScheduleRepeating(){
-        final int[] counter = {5};
-        delayTestFinish(700);
-        Timer timer=new Timer() {
-            @Override
-            public void run() {
-                counter[0]--;
-                if(counter[0]==0)
-                    finishTest();
-            }
+  public void testTimerScheduleRepeating() {
+    final int[] counter = {5};
+    delayTestFinish(700);
+    Timer timer =
+        new Timer() {
+          @Override
+          public void run() {
+            counter[0]--;
+            if (counter[0] == 0) finishTest();
+          }
         };
-        timer.scheduleRepeating(100);
-    }
+    timer.scheduleRepeating(100);
+  }
 
-    public void testTimerCancel(){
+  public void testTimerCancel() {
 
-        delayTestFinish(900);
-        Timer timer=new Timer() {
-            @Override
-            public void run() {
-               fail("should not be called after cancellation");
-            }
+    delayTestFinish(900);
+    Timer timer =
+        new Timer() {
+          @Override
+          public void run() {
+            fail("should not be called after cancellation");
+          }
         };
-        timer.scheduleRepeating(300);
+    timer.scheduleRepeating(300);
 
-        timer.cancel();
+    timer.cancel();
 
-        new Timer(){
-            @Override
-            public void run() {
-                finishTest();
-            }
-        }.schedule(600);
-    }
-
+    new Timer() {
+      @Override
+      public void run() {
+        finishTest();
+      }
+    }.schedule(600);
+  }
 }
